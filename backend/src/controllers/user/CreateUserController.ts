6f7e5a134164a67b8a590d0ef4b0373482ebca24
Gustavo <gustavoomenezes81@gmail.com>
2024-09-25 21:40:58 -0300

    Adicionando services e controllers para cadastrar pessoa com dados completos

--- backend/src/controllers/user/CreateUserController.ts
+++ backend/src/controllers/user/CreateUserController.ts
@@ -3,19 +3,27 @@ import { CreateUserService } from "../../services/user/CreateUserService";
 
 class CreateUserController {
   async handle(req: Request, res: Response) {
-    const { name, email, senha, tipoLogin, genero, dataNasc, cpf, tipo } = req.body;
+    const { nome, email, senha, tipoLogin, genero, dataNasc, cpf, tipo, cep, logradouro, numero, complemento, bairro, cidade, uf, telefones } = req.body;
 
     const createUserService = new CreateUserService();
 
     const user = await createUserService.execute({
-      name,
+      nome,
       email,
       senha,
       tipoLogin,
       genero,
       dataNasc,
       cpf,
-      tipo
+      tipo,
+      cep,
+      logradouro,
+      numero,
+      complemento,
+      bairro,
+      cidade,
+      uf,
+      telefones
     });
 
     return res.json(user);

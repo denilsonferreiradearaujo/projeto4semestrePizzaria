ee585ad5674292232b5449e71fb8ba4dfbf93c78
Denilson Ferreira de Araujo <denilson.araujo.programador@gmail.com>
2024-09-30 09:54:29 -0300

    Update routes.ts

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -10,6 +10,7 @@ import { ResetPasswordUserController } from './controllers/user/ResetPasswordUse
 import { CreateCategoryController } from "./controllers/category/CreateCategoryController";
 import { ListCategoryController } from "./controllers/category/ListCategoryController";
 import { UpdateCategoryController } from "./controllers/category/UpdateCategoryController";
+
 import { CreateProdutoController } from "./controllers/product/CreateProductController";
 
 // MiddleWares
@@ -25,12 +26,14 @@ router.get('/users/:pessoa_id', isAuthenticated, new DetailUserController().hand
 router.get('/users', isAuthenticated,  new DetailAllUserController().handle)
 router.post('/forgotPassword', new ForgotPasswordController().handle)
 router.post('/resetPassword/:token', new ResetPasswordUserController().handle)
-router.post('/createProduct', new CreateProdutoController().handle)
+
 // Rotas categoria
 router.post('/category', isAuthenticated, isAuthorized(['funcionario']) ,new CreateCategoryController().handle)
 router.get('/listCategory',  isAuthenticated, isAuthorized(['funcionario']) ,new ListCategoryController().handle)
 router.post('/updateCategory/:id',  isAuthenticated, isAuthorized(['funcionario']) ,new UpdateCategoryController().handle)
 
+// Rotas produto
+router.post('/createProduct', new CreateProdutoController().handle)
 
 
 export {router};

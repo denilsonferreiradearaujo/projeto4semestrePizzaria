f6db3009de3ab1a6c4e2e90908b986849d7b8f28
George Samuel <126299897+GeorgeSamuel92@users.noreply.github.com>
2024-10-08 19:08:52 -0300

    atualização

--- mobile/src/services/api.ts
+++ mobile/src/services/api.ts
@@ -2,7 +2,6 @@ import axios from 'axios';
 
 const api = axios.create({
     // baseURL: 'https://localhost:3333'
-    // baseURL: 'http://192.168.1.72:3333'
     baseURL: 'http://10.0.3.228:3333'
 })
 
